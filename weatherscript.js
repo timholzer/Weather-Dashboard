726ad7ae8dedb67ca6f26f9d70d77978e838eff0
Tim Holzer <timiholzer@gmail.com>
2020-10-23 21:54:59 -0600

    got the second query with the uvi from coords working

--- weatherscript.js
+++ weatherscript.js
@@ -24,6 +24,11 @@ $.ajax({
 
     console.log(response.name);
 
+    console.log(response.coord)
+
+    lat = (response.coord["lon"])
+    lon = (response.coord["lat"])
+
 
     let tempF = (response.main.temp - 273.15) * 1.80 + 32;
     console.log(Math.floor(tempF));
@@ -31,6 +36,22 @@ $.ajax({
     console.log(response.main.humidity);
 
     console.log(response.wind.speed);
+
+    var uviQueryURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + apiKey;
+    $.ajax({
+        url: uviQueryURL,
+        method: "GET"
+      })
+      .then(function (response){
+          
+        console.log(response);
+
+//today's weather icon, highs, lows, wind speed, humidity, uv index with a color indicator
+//we want the 5 day: weather icon, temperture, humidity
+
+
+      })
+
 
     })
 });
