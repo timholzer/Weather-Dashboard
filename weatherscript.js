6d610b5ffa4a09dfce9584e22e5119f57581d539
Tim Holzer <timiholzer@gmail.com>
2020-10-24 02:29:16 -0600

    got the data calls set up

--- weatherscript.js
+++ weatherscript.js
@@ -20,24 +20,11 @@ $.ajax({
   })
   .then(function (response){
 
-    console.log(response);
-
-    console.log(response.name);
-
-    console.log(response.coord)
-
-    lat = (response.coord["lon"])
-    lon = (response.coord["lat"])
+    lat = (response.coord["lat"])
+    lon = (response.coord["lon"])
 
 
-    let tempF = (response.main.temp - 273.15) * 1.80 + 32;
-    console.log(Math.floor(tempF));
-
-    console.log(response.main.humidity);
-
-    console.log(response.wind.speed);
-
-    var uviQueryURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + apiKey;
+    var uviQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial" + apiKey;
     $.ajax({
         url: uviQueryURL,
         method: "GET"
@@ -45,6 +32,40 @@ $.ajax({
       .then(function (response){
           
         console.log(response);
+        var todayHumidity = response.daily[0].humidity;
+        console.log(todayHumidity);
+        var todayUVI = response.daily[0].uvi;
+        console.log(todayUVI);
+        var todayWind = response.daily[0].wind_speed;
+        console.log(todayWind);
+        var todayIcon = response.daily[0].weather[0].icon;
+        console.log(todayIcon);
+        var todayHigh = response.daily[0].temp.max;
+        console.log(todayHigh);
+        var todayMin = response.daily[0].temp.min;
+        console.log(todayMin);
+
+
+        todayIconLink = "http://openweathermap.org/img/wn/" + todayIcon + "@2x.png";
+        console.log(todayIconLink);
+
+        
+
+        for (let i = 1; i <= 5; i++) {
+            var futurehightemp = response.daily[i].temp.max;
+            console.log(futurehightemp);
+            var futurelowtemp = response.daily[i].temp.min;
+            console.log(futurelowtemp);
+            var futureIcon = response.daily[i].weather[0].icon;
+            console.log(futureIcon);
+            var futureHumidity = response.daily[i].humidity;
+            console.log(futureHumidity);
+            futureIconLink = "http://openweathermap.org/img/wn/" + futureIcon + "@2x.png";
+
+            
+        }
+
+
 
 //today's weather icon, highs, lows, wind speed, humidity, uv index with a color indicator
 //we want the 5 day: weather icon, temperture, humidity
