949689a9537f7a30979331dc238dbf989eb9c82d
Tim Holzer <timiholzer@gmail.com>
2020-10-24 04:08:17 -0600

    got 5 day forcast to work

--- weatherscript.js
+++ weatherscript.js
@@ -1,6 +1,6 @@
-let city
 const apiKey = "&appid=94dc837cac6009ee9dec8622a9fe75ff";
-
+let date = new Date();
+let city
 
 
 
@@ -44,14 +44,38 @@ $.ajax({
         console.log(todayHigh);
         var todayMin = response.daily[0].temp.min;
         console.log(todayMin);
-
-
         todayIconLink = "http://openweathermap.org/img/wn/" + todayIcon + "@2x.png";
         console.log(todayIconLink);
+        console.log(city);
+
+        const card = $("<div>").addClass("card");
+        const cardBody = $("<div>").addClass("card-body");
+        const cityname = $("<h4>").addClass("card-title").text(city);
+        const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+        const highTemp = $("<p>").addClass("card-text").text("High: " + todayHigh + " °F");
+        const lowTemp = $("<p>").addClass("card-text").text("Low: " + todayMin + " °F");
+        const UVDanger = $("<p>").addClass("card-text").text("UV Index: " + todayUVI);    
+        const humidity = $("<p>").addClass("card-text").text("Humidity: " + todayHumidity + "%");
+        const wind = $("<p>").addClass("card-text").text("Wind Speed: " + todayWind + " MPH");
+        const image = $("<img>").attr("src", todayIconLink)
+
+        cityname.append(cityDate, image)
+        cardBody.append(cityname, highTemp, lowTemp, UVDanger, humidity, wind);
+        card.append(cardBody);
+        $("#currentCity").append(card)
+
+
+
+
 
         
 
         for (let i = 1; i <= 5; i++) {
+
+            var futureDate = new Date();
+            futureDate.setDate(futureDate.getDate() + i);
+
+
             var futurehightemp = response.daily[i].temp.max;
             console.log(futurehightemp);
             var futurelowtemp = response.daily[i].temp.min;
@@ -61,6 +85,20 @@ $.ajax({
             var futureHumidity = response.daily[i].humidity;
             console.log(futureHumidity);
             futureIconLink = "http://openweathermap.org/img/wn/" + futureIcon + "@2x.png";
+
+            const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+            const cardBody = $("<div>").addClass("card-body p-3")
+            const cityDate = $("<h4>").addClass("card-title").text(futureDate.toLocaleDateString('en-US'));
+            const highTemp = $("<p>").addClass("card-text").text("High: " + futurehightemp + " °F");
+            const lowTemp = $("<p>").addClass("card-text").text("Low: " + futurelowtemp + " °F");
+            const humidity = $("<p>").addClass("card-text").text("Humidity: " + futureHumidity + "%");
+    
+            const image = $("<img>").attr("src", futureIconLink);
+    
+            cardBody.append(cityDate, image, highTemp, lowTemp, humidity);
+            card.append(cardBody);
+            $("#5dayForecast").append(card);
+
 
             
         }
